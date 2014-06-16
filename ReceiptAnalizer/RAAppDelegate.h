c0bbc8077bfcd984c1cc77c5ec7ac4ecc29bd8b7
grampe <igrampe@gmail.com>
2014-06-16 17:55:24 +0600

    Initial Commit

---
+++ ReceiptAnalizer/RAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RAAppDelegate.h
+//  ReceiptAnalizer
+//
+//  Created by Sema Belokovsky on 16.06.14.
+//  Copyright (c) 2014 Sema Belokovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReceiptAnalizer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReceiptAnalizer
+//
+//  Created by Sema Belokovsky on 16.06.14.
+//  Copyright (c) 2014 Sema Belokovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([RAAppDelegate class]));
+	}
+}
